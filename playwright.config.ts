a77671c53942353758982b306f4ddbd390bdaf2b
isha.sharma <isha.sharma@servicenow.com>
2026-02-21 15:41:28 +0530

    headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot:'on-first-failure', 
-    headless: false,
+    headless: true,
     video: 'on',
     baseURL:"https://naveenautomationlabs.com/opencart/index.php"
     
